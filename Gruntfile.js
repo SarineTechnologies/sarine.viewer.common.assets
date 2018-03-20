33d7c1f0de7089786746ffc2a4bf7bb2f7830df0
RivkaShaked <rivkas@3base.co.il>
2018-03-20 11:45:22 +0200

    fix not to copy fonts and img folder to s3

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
             dist: {
                 files: [{
                     cwd: 'app',
-                    src: '**/*',
+                    src: ['**/*','!fonts/**','!img/**'],
                     dest: config.dist.root,
                     expand: true
                 }]
