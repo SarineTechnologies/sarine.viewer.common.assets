41f4a783704a16fcac8f49f1b0e1715bbec30fad
Meir Ben-Shimon <Meir.Ben-Shimon@sarine.com>
2018-09-20 08:40:56 +0300

    added light bars

---
+++ app/js/lightBars.js
@@ -0,0 +1,68 @@
+(function (window, document, $)
+{
+    'use strict';
+
+    window.showLightBars = function(elementID, grades, color, borderColor, thickness)
+    {
+        //using http://www.chartjs.org/
+
+        var ctx = document.getElementById(elementID).getContext('2d');
+        var barsChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                //the labels are displayed by the HTML
+                labels: ['', '', '', ''],
+                datasets: [{
+                    data: grades,
+                    backgroundColor: color,
+                    borderColor: borderColor,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    display: false,
+                    /*labels: {
+                        fontColor: 'rgb(255, 99, 132)'
+                    }*/
+                },
+                tooltips: {
+                    enabled: false
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines : {
+                            display: false,
+                        },
+                        barThickness: thickness,
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            
+                            //define labels
+                            callback: function(value, index, values)
+                            {
+                                switch(value)
+                                {
+                                    case 0:
+                                        return "";
+                                    case 1:
+                                        return "Minimum";
+                                    case 2:
+                                        return "Standard";
+                                    case 3:
+                                        return "High";
+                                    case 4:
+                                        return "Very High";
+                                    case 5:
+                                        return "Exceptional";
+                                }
+                            }
+                        }
+                    }]
+                }
+            }
+        });
+    }
+})(window, window.document, window.jQuery);
